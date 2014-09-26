17c95d3366d4e2ee5b32554bce6ab7572416fcfa
Zniwiarz44 <krystianhoro@gmail.com>
2014-09-26 15:43:52 +0100

    Convert the file to bash executable

--- cpucount.sh
+++ cpucount.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+# Title : cpucount
+# Date : Today
+# Author : Me
+# Version : 1.0
+# Description :	I like # 
+# Options : No options
+# <-- this is a comment
+
 cat /proc/cpuinfo | grep processor | wc -l
